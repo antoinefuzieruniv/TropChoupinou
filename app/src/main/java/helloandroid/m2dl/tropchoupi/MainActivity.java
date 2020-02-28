b7442c50285ea6b1a10179ed735d5100a31e8b21
Salim Cherifi <salim.cherifi@master-developpement-logiciel.fr>
2020-02-28 15:28:53 +0100

    camera working

--- app/src/main/java/helloandroid/m2dl/tropchoupi/MainActivity.java
+++ app/src/main/java/helloandroid/m2dl/tropchoupi/MainActivity.java
@@ -1,9 +1,12 @@
 package helloandroid.m2dl.tropchoupi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +20,20 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.retouche:
+                return true;
+            case R.id.prendre_photo:
+                Intent intent = new Intent(this, Camera.class);
+                startActivity(intent);
+                break;
+            case R.id.partager:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
