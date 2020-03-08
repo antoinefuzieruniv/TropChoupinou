9221604d0cf0d203b926911b9db0d5b195bb6010
raphael gouesnard <raphix.g@hotmail.fr>
2020-03-08 23:00:16 +0100

    firebase

--- app/src/main/java/helloandroid/m2dl/tropchoupi/MainActivity.java
+++ app/src/main/java/helloandroid/m2dl/tropchoupi/MainActivity.java
@@ -2,8 +2,14 @@ package helloandroid.m2dl.tropchoupi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
+import android.widget.ImageView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FireBase fireBase = new FireBase();
+        //File f = new File("test.jpg");
+
+        Bitmap bMap = BitmapFactory.decodeFile("ressource/test.jpg");
+        fireBase.putPhoto(bMap);
     }
 
     @Override
