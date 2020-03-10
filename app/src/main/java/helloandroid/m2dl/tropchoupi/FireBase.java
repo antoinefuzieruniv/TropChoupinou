2da55da3e157118e1b76a81986e86ba48aae1867
raphael gouesnard <raphix.g@hotmail.fr>
2020-03-10 23:15:44 +0100

    ajout du get all

--- app/src/main/java/helloandroid/m2dl/tropchoupi/FireBase.java
+++ app/src/main/java/helloandroid/m2dl/tropchoupi/FireBase.java
@@ -12,6 +12,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
@@ -19,15 +20,6 @@ import java.io.ByteArrayOutputStream;
 import java.util.UUID;
 
 public class FireBase {
-
-
-    public void putPhoto(Bitmap bitmap){
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-
-        DatabaseReference myRef = database.getReference("message");
-
-        myRef.setValue("Hello, World!");
-    }
             // --------------------
             // REST REQUESTS
             // --------------------
@@ -41,7 +33,7 @@ public class FireBase {
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
             byte[] data = baos.toByteArray();
-            StorageReference mImageRef = FirebaseStorage.getInstance().getReference(uuid);
+            StorageReference mImageRef = FirebaseStorage.getInstance().getReference("image").child(uuid+"coordonnée:");
             mImageRef.putBytes(data).addOnFailureListener(new OnFailureListener() {
                 @Override
                 public void onFailure(@NonNull Exception exception) {
@@ -52,11 +44,51 @@ public class FireBase {
                 public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
                     // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
                     // ...
+                    getAllPhotos();
                 }
             });
 
         }
 
+        public void getAllPhotos(){
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            StorageReference listRef = storage.getReference("image");
+
+            listRef.listAll()
+                    .addOnSuccessListener(new OnSuccessListener<ListResult>() {
+                        @Override
+                        public void onSuccess(ListResult listResult) {
+                            for (StorageReference prefix : listResult.getPrefixes()) {
+                                // All the prefixes under listRef.
+                                // You may call listAll() recursively on them.
+
+                            }
+
+                            for (StorageReference item : listResult.getItems()) {
+                                // All the items under listRef.
+                                final long ONE_MEGABYTE = 1024 * 1024;
+                                item.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                                    @Override
+                                    public void onSuccess(byte[] bytes) {
+                                        System.out.println("la");
+                                        // Data for "images/island.jpg" is returns, use this as needed
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception exception) {
+                                        // Handle any errors
+                                    }
+                                });
+                            }
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            // Uh-oh, an error occurred!
+                        }
+                    });
+        }
 
 
 }
